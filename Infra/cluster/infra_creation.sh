ded7a6725e79bbe4b3d715a469045e6209e9899b
simplon-lerouxDunvael <simplon.lerouxdunvael@gmail.com>
2023-07-04 14:23:59 +0200

    typos corrected and useless file deleted

--- Infra/cluster/infra_creation.sh
+++ Infra/cluster/infra_creation.sh
@@ -4,13 +4,13 @@
 rgname="b11duna"
 aksname="AKSClusterDuna"
 rgloc="francecentral"
-redusrtreafik="devusertreafik"
-redpasstreafik="password_redis_519"
-basicauthuser="devusertreafik"
+redusrtraefik="devusertraefik"
+redpasstraefik="password_redis_519"
+basicauthuser="devusertraefik"
 basicauthpass="password_basicauth_648"
 apitoken="xKAj86qFn5Tj6WH5T2rENi4B"
 certvers="v1.10.1"
-IngTreafik="treafik-dev"
+Ingtraefik="traefik-dev"
 
 
 # Create resource group
@@ -34,40 +34,40 @@ curl -sS https://webinstall.dev/k9s | bash
 echo "The watchdog is here."
 
 # Create Dev namespace
-echo "Creating Prod namespace for Treafik and voting-app..."
+echo "Creating Prod namespace for traefik and voting-app..."
 kubectl create namespace dev
 echo "Namespaces created"
 
 # Create Redis database secret
 echo "Creating Redis database secret for namespace dev..."
-kubectl create secret generic redis-secret-treafik --from-literal=username=$redusrtreafik --from-literal=password=$redpasstreafik -n dev
+kubectl create secret generic redis-secret-traefik --from-literal=username=$redusrtraefik --from-literal=password=$redpasstraefik -n dev
 echo "Redis database secret created."
 
-# Create Treafik authentication secret
-echo "Creating Treafik authentication secret for namespace dev..."
-kubectl create secret generic basicauth-treafik-secret --from-literal=username=$basicauthuser --from-literal=password=$basicauthpass -n dev
-echo "Treafik authentication secret created."
+# Create traefik authentication secret
+echo "Creating traefik authentication secret for namespace dev..."
+kubectl create secret generic basicauth-traefik-secret --from-literal=username=$basicauthuser --from-literal=password=$basicauthpass -n dev
+echo "traefik authentication secret created."
 
-# Create Redis database and Treafik secrets
-echo "Creating Redis database and Treafik secrets..."
+# Create Redis database and traefik secrets
+echo "Creating Redis database and traefik secrets..."
 kubectl apply -f azure-vote.yaml -n dev
-echo "Redis database and Treafik secrets created."
+echo "Redis database and traefik secrets created."
 
-# Install Treafik Ingress Controller
-echo "Installing Treafik Ingress Controller..."
+# Install traefik Ingress Controller
+echo "Installing traefik Ingress Controller..."
 helm repo add traefik https://helm.traefik.io/traefik
 helm repo update
-helm install $IngTreafik traefik/traefik -f values.yaml -n dev --debug --set controller.ingressClass="$IngTreafik"
+helm install $Ingtraefik traefik/traefik -f values.yaml -n dev --debug --set controller.ingressClass="$Ingtraefik"
 echo "Treakif Ingress Controller installed."
 
-# Break time for Treafik to initialize
-echo "Let's take 5 to let Treafik settle in..."
+# Break time for traefik to initialize
+echo "Let's take 5 to let traefik settle in..."
 sleep 30s
 echo "Alright, let's steam ahead !"
 
 # Extract External IP address
-DevIngIP=$(kubectl get svc treafik-dev-traefik -n dev -o=jsonpath='{.status.loadBalancer.ingress[0].ip}')
-echo "Treafik (dev) Ingress: $DevIngIP"
+DevIngIP=$(kubectl get svc traefik-dev-traefik -n dev -o=jsonpath='{.status.loadBalancer.ingress[0].ip}')
+echo "traefik (dev) Ingress: $DevIngIP"
 
 # Insert a pause in the script so that users can report IP to DNS
 read -n1 -r -p "Press Y to continue, or N to stop: " key
@@ -85,54 +85,43 @@ else
     :
 fi
 
-# # Add Jetstack Helm repository
-# echo "Adding Jetstack Helm repository..."
-# helm repo add jetstack https://charts.jetstack.io
-# echo "Jetstack Helm repository added."
+# Add Jetstack Helm repository
+echo "Adding Jetstack Helm repository..."
+helm repo add jetstack https://charts.jetstack.io
+echo "Jetstack Helm repository added."
 
-# # Install cert-manager with custom DNS settings
-# echo "Installing cert-manager..."
-# helm install cert-manager jetstack/cert-manager --namespace cert-manager --create-namespace --set installCRDs=true --version v1.10.1 --set 'extraArgs={--dns01-recursive-nameservers=8.8.8.8:53\,1.1.1.1:53}'
-# echo "Cert-manager installed."
+# Install cert-manager with custom DNS settings
+echo "Installing cert-manager..."
+helm install cert-manager jetstack/cert-manager --namespace cert-manager --create-namespace --set installCRDs=true --version v1.10.1 --set 'extraArgs={--dns01-recursive-nameservers=8.8.8.8:53\,1.1.1.1:53}'
+echo "Cert-manager installed."
 
-# # Install cert-manager-webhook-gandi Helm chart
-# echo "Installing cert-manager-webhook-gandi Helm chart..."
-# helm install cert-manager-webhook-gandi --repo https://bwolf.github.io/cert-manager-webhook-gandi --version v0.2.0 --namespace cert-manager --set features.apiPriorityAndFairness=true --set logLevel=6 --generate-name
-# echo "cert-manager-webhook-gandi Helm chart installed."
+# Install cert-manager-webhook-gandi Helm chart
+echo "Installing cert-manager-webhook-gandi Helm chart..."
+helm install cert-manager-webhook-gandi --repo https://bwolf.github.io/cert-manager-webhook-gandi --version v0.2.0 --namespace cert-manager --set features.apiPriorityAndFairness=true --set logLevel=6 --generate-name
+echo "cert-manager-webhook-gandi Helm chart installed."
 
-# # Apply applicative layers
-# echo "Applying applicative configuration files..."
-# kubectl apply -f azure-vote.yaml -n qua
-# kubectl apply -f azure-vote.yaml -n prod
-# echo "Applicative configuration files applied"
+# Apply Ingress layer
+echo "Applying Ingress configuration files..."
+kubectl apply -f ingress_dev1.yaml -n qua
+echo "Ingress configuration file applied."
 
-# # Apply Ingress layer
-# echo "Applying Ingress configuration files..."
-# kubectl apply -f ingress_qua1.yaml -n qua
-# kubectl apply -f ingress_prod1.yaml -n prod
-# echo "Ingress configuration files applied."
+# Apply Issuer layer
+echo "Applying Let's Encrypt Issuer configuration files..."
+kubectl apply -f issuer-dev.yaml -n dev
+echo "Let's Encrypt Issuer configuration file applied."
 
-# # Apply Issuer layer
-# echo "Applying Let's Encrypt Issuer configuration files..."
-# kubectl apply -f issuer-qua.yaml -n qua
-# kubectl apply -f issuer-prod.yaml -n prod
-# echo "Let's Encrypt Issuer configuration files applied."
+# Create Gandi API token secret
+echo "Creating Gandi API token secret..."
+kubectl create secret generic gandi-credentials --from-literal=api-token=$apitoken
+kubectl create secret generic gandi-credentials --from-literal=api-token=$apitoken -n dev
+echo "Gandi API token secret created."
 
-# # Create Gandi API token secret
-# echo "Creating Gandi API token secret..."
-# kubectl create secret generic gandi-credentials --from-literal=api-token=$apitoken
-# kubectl create secret generic gandi-credentials --from-literal=api-token=$apitoken -n qua
-# kubectl create secret generic gandi-credentials --from-literal=api-token=$apitoken -n prod
-# echo "Gandi API token secret created."
-
-# # Create role and rolebinding for accessing secrets
-# echo "Creating role and rolebinding for accessing secrets..."
-# hookID=$(kubectl get pods -n cert-manager | grep "cert-manager-webhook-gandi-" | cut -d"-" -f5)
-# kubectl create role access-secrets --verb=get,list,watch,update,create --resource=secrets -n qua
-# kubectl create role access-secrets --verb=get,list,watch,update,create --resource=secrets -n prod
-# kubectl create rolebinding --role=access-secrets default-to-secrets --serviceaccount=cert-manager:cert-manager-webhook-gandi-$hookID -n qua
-# kubectl create rolebinding --role=access-secrets default-to-secrets --serviceaccount=cert-manager:cert-manager-webhook-gandi-$hookID -n prod
-# echo "Role and rolebinding created."
+# Create role and rolebinding for accessing secrets
+echo "Creating role and rolebinding for accessing secrets..."
+hookID=$(kubectl get pods -n cert-manager | grep "cert-manager-webhook-gandi-" | cut -d"-" -f5)
+kubectl create role access-secrets --verb=get,list,watch,update,create --resource=secrets -n dev
+kubectl create rolebinding --role=access-secrets default-to-secrets --serviceaccount=cert-manager:cert-manager-webhook-gandi-$hookID -n dev
+echo "Role and rolebinding created."
 
 # # Apply Certificate layer
 # echo "Applying Certificate configuration files..."
